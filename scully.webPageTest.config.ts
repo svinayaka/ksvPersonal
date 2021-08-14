48bc9cfc8bf9c37e33f9a0951b6fd0d89f53b48f
Siddhi Vinayaka <svinayaka@onetrust.com>
2021-08-15 03:02:49 +0530

    update default project + added scully

---
+++ scully.webPageTest.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./projects/web-page-test/src",
+  projectName: "webPageTest",
+  outDir: './dist/static',
+  routes: {
+  }
+};
